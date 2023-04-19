ba87b30974ea64403b1ad818bf4bd901e8cea834
jesus castillo <jesus.castillo352@gmail.com>
2023-04-19 16:09:23 -0500

    fix: the warning of the alt property on card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,7 +4,7 @@ import '../styles/card.css';
 const Card = (props) => {
   return (
     <div className='card__container'>
-        <img src={props.url} alt="service image"  />
+        <img src={props.url} alt="service"  />
         <div>
             <h4>{props.title}</h4>
             <p>{props.description}</p>
