7ce1fe9eea4e8419272ed66d399e93798b3fc364
jesus castillo <jesus.castillo352@gmail.com>
2023-05-01 15:56:31 -0500

    feat: adding the responsive styles

--- src/components/SecondSection.jsx
+++ src/components/SecondSection.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import trabajando from '../utils/video.mp4';
-import Video from './Video';
 import '../styles/secondsection.css'
 
 const SecondSection = () => {
@@ -11,7 +9,6 @@ const SecondSection = () => {
             <p>Los plomeros especialistas de nuestra empresa estamos en capacidad de valorar, diagnosticar, prevenir, corregir y tratar todos los problemas relacionados con la tubería, <strong>fugas y filtraciones</strong> , desagüé y <strong>destapes de cañerías</strong>, todos nuestros clientes están satisfechos y cumplimos con el 100% de calidad.<br/><br/>
             Nuestra empresa cumple con todas las normas para brindarte el mejor servicio de plomería en Chía, Cajicá y toda la Sabana de Bogotá.</p>
         </div>
-        <Video video = {trabajando} format= 'video/mp4'/>
     </section> )
 }
 
